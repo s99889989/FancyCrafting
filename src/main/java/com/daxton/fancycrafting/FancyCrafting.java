734d5793d76432432a71638aa68d049583fdfab4
Daxton <s02048898067@gmail.com>
2021-07-27 06:58:28 +0800

    1

---
+++ src/main/java/com/daxton/fancycrafting/FancyCrafting.java
@@ -0,0 +1,17 @@
+package com.daxton.fancycrafting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FancyCrafting extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
